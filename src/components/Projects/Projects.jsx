e86cf07978bf894788ac47d6f87750d47330c773
spbovarnick <spbovarnick@gmail.com>
2023-05-22 16:58:15 -0700

    added WAF to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,59 +3,54 @@ import FadeInSection from '../FadeInSection/FadeInSection'
 import AnomAnonMockup from '../../assets/AnomAnonMockup.png'
 import DriftCastMockup from '../../assets/DriftCastMockup.png'
 import FlyBinMockup from '../../assets/FlyBinMockup.png'
+import WAFMockup from '../../assets/WAFMockup.png'
+import globe from '../../assets/icons/globe.jpeg'
+import github from '../../assets/icons/github.svg'
 import './projects.scss'
-
-// const FadeInSection = (props) => {
-//     const [isVisible, setIsVisible] = useState(false)
-//     const domRef = useRef(null)
-//     useEffect(() => {
-//         const observer = new IntersectionObserver(entries => {
-//             entries.forEach(entry => setIsVisible(entry.isIntersecting))
-//         })
-//         observer.observe(domRef.current)
-//         // return () => observer.unobserve(domRef.current)
-//     }, [])
-
-//     return (
-//         <div className={`card fade-in-section ${isVisible ? 'is-visible' : ''}`} ref={domRef}>
-//             {props.children}
-//         </div>
-//     )
-// }
 
 export default function Projects() {
 
     return (
         <section id='projects' > 
             <FadeInSection>
-                {/* <div className='card'> */}
-                    <img src={DriftCastMockup} />
+                <div className='card'>
+                    <img src={WAFMockup} className='mockup' />
+                    <h2>World Arts Foundation - React and Rails Digital Archive</h2>
+                    <div className="links">
+                        <a href='https://www.worldartsfoundation.org/archive-beta'><img src={globe} className='icon' /> Live Site</a>
+                        <a href='https://github.com/leeritter/waf'><img src={github} className='icon' /> GitHub Repo</a>
+                    </div>
+                </div>
+            </FadeInSection>
+            <FadeInSection>
+                <div className='card'>
+                    <img src={DriftCastMockup} className='mockup' />
                     <h2>DriftCast - River Condition Monitoring MERN App</h2>
                     <div className="links">
-                        <a href='https://drift-cast.herokuapp.com/' target='_blank'><i className="fa-solid fa-globe fa-xl"></i> Live Site</a>
-                        <a href='https://github.com/spbovarnick/drift-cast' target='_blank'><i className="fa-brands fa-github fa-xl"></i> GitHub Repo</a>
+                        <a href='https://drift-cast.herokuapp.com/' target='_blank'><img src={globe} className='icon' /> Live Site</a>
+                        <a href='https://github.com/spbovarnick/drift-cast' target='_blank'><img src={github} className='icon' /> GitHub Repo</a>
                     </div>
-                {/* </div> */}
+                </div>
             </FadeInSection>
             <FadeInSection>
-                {/* <div className='card'> */}
-                    <img src={AnomAnonMockup} />
+                <div className='card'>
+                    <img src={AnomAnonMockup} className='mockup' />
                     <h2>AnomaliesAnonymous - UFO Sighting Django Project</h2>
                     <div className="links">
-                        <a href='https://www.anomaliesanonymous.com/' target='_blank'><i className="fa-solid fa-globe fa-xl"></i> Live Site</a>         
-                        <a href='https://github.com/spbovarnick/anomalies-anonymous' target='_blank'><i className="fa-brands fa-github fa-xl"></i> GitHub Repo</a>           
+                        <a href='https://www.anomaliesanonymous.com/' target='_blank'><img src={globe} className='icon' /> Live Site</a>         
+                        <a href='https://github.com/spbovarnick/anomalies-anonymous' target='_blank'><img src={github} className='icon' /> GitHub Repo</a>           
                     </div>
-                {/* </div> */}
+                </div>
             </FadeInSection>
             <FadeInSection>
-                {/* <div className='card'> */}
-                    <img src={FlyBinMockup} />
+                <div className='card'>
+                    <img src={FlyBinMockup} className='mockup' />
                     <h2>FlyBin - Fly Organizing MEN App</h2>
                     <div className="links">
-                        <a href='https://fly-bin.herokuapp.com/' target='_blank'><i className="fa-solid fa-globe fa-xl"></i> Live Site</a>
-                        <a href='https://github.com/spbovarnick/fly-bin' target='_blank'><i className="fa-brands fa-github fa-xl"></i> GitHub Repo</a>                    
+                        <a href='https://fly-bin.herokuapp.com/' target='_blank'><img src={globe} className='icon' /> Live Site</a>
+                        <a href='https://github.com/spbovarnick/fly-bin' target='_blank'><img src={github} className='icon' /> GitHub Repo</a>                    
                     </div>
-                {/* </div> */}
+                </div>
             </FadeInSection>
         </section>
     )
